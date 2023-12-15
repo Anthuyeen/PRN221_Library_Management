cd90474590426c61fc2912e39dfcb8c89b474838
Anthuyeen <87635968+Anthuyeen@users.noreply.github.com>
2023-12-15 07:42:45 +0700

    last but not least

---
+++ Logics/BookInfoManager.cs
@@ -0,0 +1,57 @@
+﻿using Project_Library_Management_FA23_BL5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Library_Management_FA23_BL5.Logics
+{
+    internal class BookInfoManager
+    {
+        public static List<BookInfo> GetBookInfos()
+        {
+            using var context = new LibraryManagementContext();
+            return context.BookInfos.ToList();
+        }
+
+        public static List<BookInfo> GetBookInfos(int titleId)
+        {
+            using var context = new LibraryManagementContext();
+            return context.BookInfos.Where(x => x.TitleId == titleId).ToList();
+        }
+
+        public static BookInfo? GetBookInfo(int titleId)
+        {
+            using var context = new LibraryManagementContext();
+            return context.BookInfos.FirstOrDefault(x => x.TitleId == titleId);
+        }
+
+        public static BookInfo? GetBookInfo(string title)
+        {
+            using var context = new LibraryManagementContext();
+            return context.BookInfos.FirstOrDefault(x => x.Title.ToUpper() == title.ToUpper());
+        }
+
+        public static void AddBookInfo(BookInfo data)
+        {
+            using var context = new LibraryManagementContext();
+            context.BookInfos.Add(data);
+            context.SaveChanges();
+        }
+
+        public static void UpdateBookInfo(BookInfo data)
+        {
+            using var context = new LibraryManagementContext();
+            context.BookInfos.Update(data);
+            context.SaveChanges();
+        }
+
+        public static void DeleteBookInfo(List<BookInfo> data)
+        {
+            using var context = new LibraryManagementContext();
+            context.BookInfos.RemoveRange(data);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Logics/BookManager.cs
@@ -0,0 +1,18 @@
+﻿using Project_Library_Management_FA23_BL5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Library_Management_FA23_BL5.Logics
+{
+    internal class BookManager
+    {
+        public static Book? GetBook(int bookId)
+        {
+            using var context = new LibraryManagementContext();
+            return context.Books.FirstOrDefault(x => x.BookId == bookId);
+        }
+    }
+}
